42c8534c12983462986fa28a9810498e426edfe3
Mrutyunjaya Behera <55201679+MrutyunjayaBehera@users.noreply.github.com>
2020-05-19 17:25:18 +0530

    Update hrank.py

--- hrank.py
+++ hrank.py
@@ -1,3 +1,18 @@
+#THIS IS THE FIRST PROBLEM IN HACKERRANK PYTHON IF-ELSE SET
+
+#Task
+#Given an integer, , perform the following conditional actions:
+
+#If  is odd, print Weird
+#If  is even and in the inclusive range of 2 to 5, print Not Weird
+#If  is even and in the inclusive range of 5 to 20 , print Weird
+#If  is even and greater than 20 , print Not Weird
+
+# The only catch in the problem is, the input function gives a string which can not go through arithmetic operations
+# so i converted the string to integer in line 25, then everything is easy
+
+
+
 import math
 import os
 import random
@@ -19,3 +34,6 @@ else:
 		print("Weird")
 	elif n > 20:
 		print("Not Weird")
+		
+		
+#ENJOY
